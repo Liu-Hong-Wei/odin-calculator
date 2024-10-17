a3e9fa8c243181e9323b084d4438889bccc8f1cd
homeylau <lhw1953927853@gmail.com>
2024-10-17 23:36:02 +0800

    feat: add listener to number-container and display clicked number on the display-container

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberBtnContainer = document.querySelector(".number-container");
+const operatorBtnContainer = document.querySelector(".opertor-container");
+let currentNumber;
+numberBtnContainer.addEventListener("click", (e) => {
+  let number;
+  if (e.target.className == "number-btn") {
+    number = Number(e.target.firstChild.textContent);
+  } else if (e.target.parentNode.className == "number-btn") {
+    number = Number(e.target.textContent);
+  }
+  if (number != null) {
+    const displayNumber = document.querySelector(".display-container");
+    if (displayNumber.textContent.length >= 16) {
+      alert("Too much digits!");
+    } else {
+      displayNumber.textContent += number;
+    }
+  }
+});

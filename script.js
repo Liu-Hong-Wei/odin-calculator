6e2dcce8240404e426447652553d6e46ac8cb10b
homeylau <lhw1953927853@gmail.com>
2024-10-18 10:35:48 +0800

    feat: add button hover animation effect

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const numberBtnContainer = document.querySelector(".number-container");
-const operatorBtnContainer = document.querySelector(".opertor-container");
+const operatorBtnContainer = document.querySelector(".operator-container");
 let currentNumber;
 numberBtnContainer.addEventListener("click", (e) => {
   let number;
-  if (e.target.className == "number-btn") {
+  if (e.target.className == "number-btn btn") {
     number = Number(e.target.firstChild.textContent);
-  } else if (e.target.parentNode.className == "number-btn") {
+  } else if (e.target.parentNode.className == "number-btn btn") {
     number = Number(e.target.textContent);
   }
   if (number != null) {
     const displayNumber = document.querySelector(".display-container");
-    if (displayNumber.textContent.length >= 16) {
+    if (displayNumber.textContent.length >= 18) {
       alert("Too much digits!");
     } else {
       displayNumber.textContent += number;
